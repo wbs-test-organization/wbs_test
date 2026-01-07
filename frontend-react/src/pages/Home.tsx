01c1a6d71a58e42ef84e1c75fb55e513efd8c0df
Viet Nhan <vietnhan033@gmail.com>
2026-01-07 16:51:50 +0700

    fix status

--- frontend-react/src/pages/Home.tsx
+++ frontend-react/src/pages/Home.tsx
@@ -351,7 +351,7 @@ const Home: React.FC = () => {
                                         Status
                                     </label>
                                     <select
-                                        name="projectStatusName"
+                                        name="projectStatusId"
                                         value={formData.projectStatusId}
                                         onChange={handleInputChange}
                                         className="w-full px-4 py-3 border border-gray-300 dark:border-gray-600 rounded-xl bg-white dark:bg-gray-700 text-gray-900 dark:text-gray-100 focus:outline-none focus:ring-2 focus:ring-green-500"
